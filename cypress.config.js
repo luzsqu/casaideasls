7ceb3d961f6383bf926f3a8f4aec870ca0f7363b
luz squarzon <squarzonrp@gmail.com>
2024-09-23 13:23:55 -0500

    register y login

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require("Cypress");
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
-
+  defaultCommandTimeout: 20000,
+  pageLoadTimeout: 20000,
+  log: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
